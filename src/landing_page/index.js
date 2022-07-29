e0e5ce7c395c702ab81dbb0aeacb1be1f21683c7
Restyled.io <commits@restyled.io>
2022-07-29 17:59:36 +0000

    Restyled by prettier

--- src/landing_page/index.js
+++ src/landing_page/index.js
@@ -1,8 +1,10 @@
 const options__li = document.querySelectorAll(".options li");
-options__li.forEach(li => { li.addEventListener("click", changeTab); })
+options__li.forEach((li) => {
+  li.addEventListener("click", changeTab);
+});
 function changeTab() {
   for (let i = 0; i < options__li.length; i++) {
-    options__li[i].classList.remove('active');
+    options__li[i].classList.remove("active");
   }
-  this.classList.add('active')
+  this.classList.add("active");
 }
